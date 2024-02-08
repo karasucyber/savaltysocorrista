42005e51fab49b4a906c611cf0f070b6293a6cd8
karasu_corp <aquillae777@gmail.com>
2024-02-07 21:55:51 -0300

    carrousel + container geral add componentes

---
+++ socorrista/Componentes/Geral/geral/ContainerGeral.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.div({
+    width: "1400px",
+    height: "1000px",
+    display: "flex",
+    justifyContent: "center",
+})
+
+
+export const Titulo = styled.h1({
+    fontSize: "50px",
+    color: "Background",
+})
+
+
+export default Container
--- socorrista/app/page.tsx
+++ socorrista/app/page.tsx
@@ -1,10 +1,20 @@
+'use client'
+import styled from "styled-components";
+import Container1 from "@/Componentes/Container1/Container1";
+
+const Body = styled.body({
+  width: "100%",
+  height: "100%",
+  padding: "0px",
+  margin: "0px",
+  left: "0px",
+  background: "#BDE3FF",
+})
 
 export default function Home() {
   return (
-    <main className="">
+    <Body>
 
-
-
-    </main>
+    </Body>
   );
 }

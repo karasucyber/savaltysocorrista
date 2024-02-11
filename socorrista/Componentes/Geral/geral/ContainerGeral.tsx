4af0f759415e2449b2b066d68b508dd3f5abc311
karasu_corp <aquillae777@gmail.com>
2024-02-11 01:05:08 -0300

    up config deploy

--- socorrista/Componentes/Geral/geral/ContainerGeral.tsx
+++ socorrista/Componentes/Geral/geral/ContainerGeral.tsx
@@ -7,53 +7,57 @@ export const Container = styled.div({
     display: "flex",
     justifyContent: "center",
     background: "#BDE3FF",
-    '@media only screen and (min-width: 780px)': {
+    '@media only screen and (max-width: 780px)': {
+        // Estilos específicos para telas menores que 780px aqui
     }
 });
+
 export const ContainerLeft = styled.div({
     display: "flex",
     justifyItems: "flex-start",
     width: "500px",
     height: "500px",
     padding: "10px",
-    '@media only screen and (min-witdh: 780px)':{
-
+    '@media only screen and (max-width: 780px)': {
+        // Estilos específicos para telas menores que 780px aqui
     }
-})
+});
+
 export const ContainerRight = styled.div({
     display: "flex",
     justifyItems: "flex-end",
     width: "500px",
     height: "500px",
     padding: "10px",
-    '@media only screen and (min-witdh: 780px)':{
-
+    '@media only screen and (max-width: 780px)': {
+        // Estilos específicos para telas menores que 780px aqui
     }
-})
+});
+
 export const Titulo = styled.h1({
     fontSize: "90px",
     color: "Background",
     marginBottom: "20px",
-    '@media only screen and (min-witdh: 780px)':{
+    '@media only screen and (max-width: 780px)': {
         fontSize: "20px",
-
     }
-})
+});
+
 export const Paragrafo = styled.p({
     fontSize: "25px",
     background: "black",
-    '@media only screen and (min-witdh: 780px)':{
-
+    '@media only screen and (max-width: 780px)': {
+        // Estilos específicos para telas menores que 780px aqui
     }
-})
+});
+
 export const BoxShaw = styled.div({
     width: "500px",
     height: "500px",
     boxShadow: "",
     padding: "10px",
     margin: "10px",
-    '@media only screen and (min-witdh: 780px)':{
-
+    '@media only screen and (max-width: 780px)': {
+        // Estilos específicos para telas menores que 780px aqui
     }
-})
-
+});
